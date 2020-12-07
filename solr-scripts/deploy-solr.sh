c81e4d19875e89e2c44adf3cd277806f2f7b3b21
Kord Campbell <kordless@gmail.com>
2020-12-07 08:33:04 -0800

    lines

--- solr-scripts/deploy-solr.sh
+++ solr-scripts/deploy-solr.sh
@@ -59,12 +59,10 @@ else
   /etc/init.d/rng-tools restart
 
   # files
-  cat "
-  solr hard nofile 65535
-  solr soft nofile 65535
-  solr hard nproc 65535
-  solr soft nproc 65535
-  " >> /etc/security/limits.conf
+  cat "solr hard nofile 65535" >> /etc/security/limits.conf
+  cat "solr soft nofile 65535" >> /etc/security/limits.conf
+  cat "solr hard nproc 65535" >> /etc/security/limits.conf
+  cat "solr soft nproc 65535" >> /etc/security/limits.conf
 
   apt-get install openjdk-11-jdk -y
   echo JAVA_HOME="/usr/lib/jvm/java-11-openjdk-amd64" >> /etc/environment
