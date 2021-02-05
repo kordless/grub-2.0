002eaef2ba94a26c88b389bdcee8ddb52096729b
Kord Campbell <kordless@gmail.com>
2021-02-05 15:41:29 -0800

    funny problems with content-type

--- aperture/BrowserSession.py
+++ aperture/BrowserSession.py
@@ -14,106 +14,104 @@ import requests
 
 
 def random_string(size=6, chars=string.ascii_letters + string.digits):
-    return ''.join(random.choice(chars) for _ in range(size))
+	return ''.join(random.choice(chars) for _ in range(size))
 
 
 class BrowserSession:
-    
-    def __init__(self, url=None, persistent=False, debug=False):
+	
+	def __init__(self, url=None, persistent=False, debug=False):
 
-        self.debug = debug
-        self.url = url
-        self.stayopen = False
-        self.headless = False
-        self.fullscreen = True
-        self.local_db = None
-        self.local_index = None
-        self.save_text = False
+		self.debug = debug
+		self.url = url
+		self.stayopen = False
+		self.headless = False
+		self.fullscreen = True
+		self.local_db = None
+		self.local_index = None
+		self.save_text = False
 
-    def setup_session(self):
+	def setup_session(self):
 
-        self.config = json.loads(open('aperture/config.json', 'r').read()) 
+		self.config = json.loads(open('aperture/config.json', 'r').read()) 
 
-        if self.headless:
-            self.config['capabilities']['alwaysMatch']['moz:firefoxOptions']['args'].insert(1,'--headless')
-            self.config['capabilities']['alwaysMatch']['moz:firefoxOptions']['args'].insert(1,'--height=1080')
-            self.config['capabilities']['alwaysMatch']['moz:firefoxOptions']['args'].insert(1,'--width=1920')
+		if self.headless:
+			self.config['capabilities']['alwaysMatch']['moz:firefoxOptions']['args'].insert(1,'--headless')
+			self.config['capabilities']['alwaysMatch']['moz:firefoxOptions']['args'].insert(1,'--height=1080')
+			self.config['capabilities']['alwaysMatch']['moz:firefoxOptions']['args'].insert(1,'--width=1920')
 
-        self.session = webdriver.Session(self.config['webdriverip'], self.config['webdriverport'], capabilities=self.config['capabilities'])
-        return
+		self.session = webdriver.Session(self.config['webdriverip'], self.config['webdriverport'], capabilities=self.config['capabilities'])
+		return
 
-        
+		
 
-    def go_to_url(self,url=None,fullscreen=True):
+	def go_to_url(self,url=None,fullscreen=True):
 
-        if url is None:
-            url = self.url 
-        
-        self.session.url = url
-        if fullscreen:
-            self.fullscreen=True
-            self.session.window.fullscreen()
-        if self.debug:
-            print("WebDriver to sessionID -------> {}".format(self.session.session_id))
+		if url is None:
+			url = self.url 
+		
+		self.session.url = url
+		if fullscreen:
+			self.fullscreen=True
+			self.session.window.fullscreen()
+		if self.debug:
+			print("WebDriver to sessionID -------> {}".format(self.session.session_id))
 
-        return
+		return
 
 
-    def save_screenshot(self,filename=None):
-        if filename is None:
-            filename = "%s.png" % random_string(23)
+	def save_screenshot(self,filename=None):
+		if filename is None:
+			filename = "%s.png" % random_string(23)
 
-            if self.debug:
-                print("filename="+filename)
-                print("in debug")
+			if self.debug:
+				print("filename="+filename)
+				print("in debug")
 
-        try:
-            if self.fullscreen:
-                r = requests.get(url="http://localhost:4444/session/" + self.session.session_id + "/moz/screenshot/full")
-            else:
-                r = requests.get(url="http://localhost:4444/session/" + self.session.session_id + "/screenshot")
-            if r.status_code == 200:
-                try:
-                    with open("/opt/grub-2.0/aperture/images/%s" % filename, 'wb') as screenshot:
-                        screenshot.write(base64.b64decode(r.json()['value']))
-                except IOError as err:
-                    print("I/O error: {0}".format(err))
-            elif r.status_code == 404:
-                if self.debug:
-                    print("Something is wrong with the session? maybe it's closed????")
-                    print(r.json())
+		try:
+			if self.fullscreen:
+				r = requests.get(url="http://localhost:4444/session/" + self.session.session_id + "/moz/screenshot/full")
+			else:
+				r = requests.get(url="http://localhost:4444/session/" + self.session.session_id + "/screenshot")
+			if r.status_code == 200:
+				try:
+					with open("/opt/grub-2.0/aperture/images/%s" % filename, 'wb') as screenshot:
+						screenshot.write(base64.b64decode(r.json()['value']))
+				except IOError as err:
+					print("I/O error: {0}".format(err))
+			elif r.status_code == 404:
+				if self.debug:
+					print("Something is wrong with the session? maybe it's closed????")
+					print(r.json())
 
-        except Exception:
-            print("got exception")
-            traceback.print_exc()
-            pass
+		except Exception:
+			print("got exception")
+			traceback.print_exc()
+			pass
 
-        # upload to the spool endpoint
-        # always return image/png
-        headers = {'Content-type': 'image/png'}
-        with open("/opt/grub-2.0/aperture/images/%s" % filename.rstrip('\r\n'),'rb') as filedata:
-            appengine_response = requests.post(
-                "%s?token=%s" % (sys.argv[2], sys.argv[3]),
-                files={'data': filedata},
-                headers=headers
-            )
-        return filename
-    
-        
+		# upload to the spool endpoint
+		# always return image/png
+		with open("/opt/grub-2.0/aperture/images/%s" % filename.rstrip('\r\n'),'rb') as filedata:
+			appengine_response = requests.post(
+				"%s?token=%s" % (sys.argv[2], sys.argv[3]),
+				files={'data': filedata, 'content-type': 'image/png'},
+			)
+			return appengine_response.text
+	
+		
 def main():
-    try:
-        new_session = BrowserSession()
-        new_session.headless = True
-        new_session.setup_session()
+	try:
+		new_session = BrowserSession()
+		new_session.headless = True
+		new_session.setup_session()
 
-        new_session.go_to_url(sys.argv[1],fullscreen=True)
-        time.sleep(2)
-        filename = new_session.save_screenshot()
-        
-        print(filename)
+		new_session.go_to_url(sys.argv[1],fullscreen=True)
+		time.sleep(2)
+		filename = new_session.save_screenshot()
+		
+		print(filename)
 
-    except Exception as ex:
-        print("error %s" % ex)
+	except Exception as ex:
+		print("error %s" % ex)
 
 if __name__ == '__main__':
-    main()
+	main()
