cb8f941c7468bfc73c4f956d56286b1e3963e9be
Kord Campbell <kordless@gmail.com>
2022-01-21 11:13:31 -0600

    fixing stuff

--- fastener/main.py
+++ fastener/main.py
@@ -15,7 +15,7 @@ from googleapiclient import discovery
 from bottle import Bottle, route, run, template, response, request, redirect, error
 
 # generators
-def id_generator(size=4, chars=string.ascii_lowercase + "labs"):return ''.join(random.choice(chars) for _ in range(size))
+def id_generator(size=4, chars=string.ascii_lowercase + "solr"):return ''.join(random.choice(chars) for _ in range(size))
 def password_generator(size=12, chars=string.ascii_lowercase + string.digits):return ''.join(random.choice(chars) for _ in range(size))
 
 # get the token from gcp tag on instance
