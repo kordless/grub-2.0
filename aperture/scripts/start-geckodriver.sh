7bf01e60ceb010f94d0598678834b4c7b6bdb75b
Kord Campbell <kordless@gmail.com>
2022-02-16 13:28:39 -0600

    silly scaling things

--- aperture/scripts/start-geckodriver.sh
+++ aperture/scripts/start-geckodriver.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 cd /opt/grub-2.0/aperture/
-./geckodriver -v --marionette-port 2928 -p 4444 &
-./geckodriver -v --marionette-port 2929 -p 4445 &
-./geckodriver -v --marionette-port 2930 -p 4446 &
-./geckodriver -v --marionette-port 2931 -p 4447 &
-./geckodriver -v --marionette-port 2932 -p 4448 &
+
+screen -dmS geckodriver4 bash -c "./geckodriver -v --marionette-port 2928 -p 4444"
+screen -dmS geckodriver5 bash -c "./geckodriver -v --marionette-port 2929 -p 4445"
+screen -dmS geckodriver6 bash -c "./geckodriver -v --marionette-port 2930 -p 4446"
+screen -dmS geckodriver7 bash -c "./geckodriver -v --marionette-port 2931 -p 4447"
+screen -dmS geckodriver8 bash -c "./geckodriver -v --marionette-port 2932 -p 4448"
